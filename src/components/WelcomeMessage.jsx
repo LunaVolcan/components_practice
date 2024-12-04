aa1a8fcdf456c7d82712c87133c4fc762336b791
Luna Fernandez <lunafernandez222@gmail.com>
2024-12-04 12:09:24 -0700

    first commit

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,4 @@
+export default function WelcomeMessage({name}) {
+    return (
+        <h1>hello {name}!</h1>
+    )}
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,9 @@
+import WelcomeMessage from "../components/WelcomeMessage";
+
+function AboutUs() {
+
+    return (
+        <WelcomeMessage name="Luna" />
+    )}
+
+export default AboutUs;
